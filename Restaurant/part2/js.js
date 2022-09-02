f6fb04a10a6b4b7ae0ff1916aa91345d671fe3bf
vikingcode12 <101129263+vikingcode12@users.noreply.github.com>
2022-09-02 09:36:36 -0500

    Initial commit

---
+++ Restaurant/part2/js.js
@@ -0,0 +1,20 @@
+function submitOrder() {
+    var ouput = '<h2> Your order has been submitted </h2>';
+    document.getElementById('idOrderSummary').innerHTML = ouput;
+}
+
+function changeImage() {
+    const appPicArray = [],
+    mainPicArray = [],
+    bevPicArray = []
+    if(this.id == 'idAppetizer') {
+        idAppPic
+    }
+    else if(this.id == 'idMainCourse') {
+        idMainPic
+    }
+    else if(this.id == 'idBeverage') {
+        
+        idBevPic.src
+    }
+}
