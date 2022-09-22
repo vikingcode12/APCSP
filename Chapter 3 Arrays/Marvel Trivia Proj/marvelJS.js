9b6ae35c66420307114decdce21d38d4761a52e5
vikingcode12 <101129263+vikingcode12@users.noreply.github.com>
2022-09-22 10:04:14 -0500

    added

---
+++ Chapter 3 Arrays/Marvel Trivia Proj/marvelJS.js
@@ -0,0 +1,15 @@
+var imageArray = ["./posters/blackpanther.jpg", "./posters/blackwidow.jpg", "./posters/captainamerica.jpg", "./posters/captainmarvel.jpg", "./posters/guardians.jpg", "./posters/shangchi.jpg"]
+var movieArray = ["Black Panther", "Black Widow", "Captain America", "Captain Marvel", "Guardians of The Galaxy", "Shang Chi"]
+var yearArray = [2018, 2021, 2011, 2019, 2014, 2021]
+
+function displayRandomQuestion() {
+
+}
+
+function checkAnswer() {
+    
+}
+
+function formatImage() {
+    
+}
