3ebd715ca777cf46e7e3dbb9489fdf3b3f2a25a5
Blocky7277 <57508798+Blocky7277@users.noreply.github.com>
2022-11-20 21:26:39 -0600

    challenge

--- Personal Projects/Uncle Challenge/wheather.py
+++ Personal Projects/Uncle Challenge/wheather.py
@@ -1,5 +1,60 @@
-# importing the library
+# importing the libraries
 import requests
 from bs4 import BeautifulSoup
+from uszipcode import SearchEngine
+import sys
+from math import isnan
 
-zip = input("What is your zipcode")
+
+engine = SearchEngine()
+
+zip = input("Input zipcode: ")
+try:
+  zip = int(zip)
+except:
+  print("Please pass through a number")
+  sys.exit()
+
+zipcode = engine.by_zipcode(zip)
+
+try:
+  city=zipcode.major_city
+except:
+  print("Please pass through a valid zipcode")
+  sys.exit()
+
+
+url = "https://www.google.com/search?q="+"weather"+city
+
+html = requests.get(url).content
+
+soup = BeautifulSoup(html, 'html.parser')
+
+temperature = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+
+# this contains time and sky description
+str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
+
+# format the data
+data = str.split('\n')
+time = data[0]
+sky = data[1]
+
+listdiv = soup.findAll('div', attrs={'class': 'BNeawe s3v9rd AP7Wnd'})
+
+# FIND GENERAL CONDITIONS
+
+# particular list with required data
+strd = listdiv[30].text
+tempStr = listdiv[1].text
+tempArr = tempStr.split('.')
+generalConditions = tempArr[0].lower()
+tempHigh = tempArr[1]
+
+# formatting the string
+pos = strd.find('Wind')
+
+print("It's", generalConditions)
+print("Temperature is", temperature, " ", tempHigh)
+print("Time: ", time)
+print("Sky: ", sky)
---
+++ Personal Projects/Uncle Challenge/wheatherApp.py
@@ -0,0 +1,99 @@
+# Create a program with a GUI that can fetch weather from zipcode using google api (NGL I would've much rather used react JS)
+
+# Imports
+import tkinter as tk;
+
+# Root of app
+root = tk.Tk()
+
+# Set app dimensions
+root.geometry(f'{640}x{480}')
+
+# Create a label widget and spacer
+label1 = tk.Label(root, text="Welcome To Weather Fetcher")
+label2 = tk.Label(root, text=" ")
+label1.pack()
+label2.pack()
+
+# Emulate placeholder for input[text] in HTML
+def placeHolder(e):
+    zip = input.get()
+    if zip == "ZIPCODE":
+        input.delete(0, len(zip))
+
+input = tk.Entry(root, width=50, justify='center')
+input.bind("<Button-1>", placeHolder)
+input.insert(0, "ZIPCODE")
+input.pack()
+
+# Actual function that fetches the weather
+def fetchWeather():
+    zip = input.get()
+    city = ''
+    import requests
+    from bs4 import BeautifulSoup
+    from uszipcode import SearchEngine
+    engine = SearchEngine()
+    output = ""
+    
+    try:
+        zip = int(zip)
+    except:
+        output = "Please input a number"
+        outputLabel.config(text=output)
+        return
+    zipcode = engine.by_zipcode(zip)
+
+    try:
+        city=zipcode.major_city
+    except:
+        output = "Please input a valid zipcode"
+        outputLabel.config(text=output)
+        return
+
+    url = "https://www.google.com/search?q="+"weather"+city
+
+    html = requests.get(url).content
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    temperature = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+
+    # this contains time and sky description
+    str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
+
+    # format the data
+    data = str.split('\n')
+    time = data[0]
+    sky = data[1]
+
+    listdiv = soup.findAll('div', attrs={'class': 'BNeawe s3v9rd AP7Wnd'})
+
+
+    # particular list with required data
+    tempStr = listdiv[1].text
+    tempArr = tempStr.split('.')
+    print(listdiv)
+    generalConditions = tempArr[0].lower()
+    tempHigh = tempArr[1]
+    output = "It's " + generalConditions + "\n"
+    output += "Temperature is " + temperature + " " + tempHigh + "\n"
+    output += "Time: " + time + "\n"
+    output += "Sky: " + sky + "\n"
+    outputLabel.config(text=output)
+
+label3 = tk.Label(root, text=" ")
+label3.pack()
+
+
+button = tk.Button(root, text="Fetch Weather", command=fetchWeather)
+button.pack()
+
+label4 = tk.Label(root, text=" ")
+label4.pack()
+
+outputLabel = tk.Label(root, text=" ")
+outputLabel.pack()
+
+root.winfo_toplevel().title("Weather Fetcher")
+root.mainloop()
