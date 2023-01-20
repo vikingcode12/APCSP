ee6ab75ca2e67d0528cae75c9ec44357f37d59d5
vikingcode12 <101129263+vikingcode12@users.noreply.github.com>
2023-01-20 10:10:42 -0600

    vigenere

--- Chapter 7 encryption/index.js
+++ Chapter 7 encryption/index.js
@@ -6,7 +6,8 @@ function fillSelect() {
     // Add 26 keys to the select box
     for (let i = 0; i < 26; i++) {
         output += `<option value="${i+1}">${i+1}</option>`
-    }   
+    }
+    output += `<option value="vig">Vigenère</option>`
     idSelectKey.innerHTML = output
 }
 
@@ -18,6 +19,11 @@ function fillSelect() {
  */
 function encrypt(iStr, key) {
     let string = iStr.toUpperCase();
+    if (key == "vig") {
+        vigKey = idVigKey.value;
+        vigEncrypt(string, vigKey);
+        return
+    }
     let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     let outputStr = ''
     for (let i = 0; i < string.length; i++) {
@@ -29,6 +35,7 @@ function encrypt(iStr, key) {
             if(ele1 == ele2) {
                 // If the letters match then add the key from index J then set that as the new index
                 let newIndex = j + Number(key)
+                // Wrap around if the index is greater than 25
                 if (newIndex > letters.length - 1) newIndex = newIndex - letters.length
                 outputStr += letters[newIndex]
                 // Break and start looking for the next letter
@@ -49,7 +56,7 @@ function encrypt(iStr, key) {
  * 
  * @param {string} iStr 
  * @param {number} key 
- */
+*/
 function decrypt(iStr, key) {
     let string = iStr.toUpperCase();
     let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -63,6 +70,7 @@ function decrypt(iStr, key) {
             if(ele1 == ele2) {
                 // If the letters match then subtract the key from index J then set that as the new index
                 let newIndex = j - Number(key)
+                // Wrap around if the index is less than 0
                 if (newIndex < 0) newIndex = newIndex + letters.length
                 // Append that to the output string
                 outputStr += letters[newIndex]
@@ -111,6 +119,54 @@ function toggleInstructions() {
     if (!txt) idInstructions.innerHTML = "Type a string in the input and select a key<br>Press Encrypt to encrypt the string with a ceaser cipher using the selected key<br>Press Decrypt to decrypt the string with a ceaser cipher using the selected key"
     // If there is hide them
     else idInstructions.innerHTML = ''
+}
+
+/**
+ * Function that checks if the key is Vigenère
+ */
+function checkKey() {
+    let key = idSelectKey.value
+    if (key == 'vig') {
+        idVigKey.hidden = false
+    }
+    else {
+        idVigKey.hidden = true
+    }
+}
+
+/**
+ * Function that checks if the key is Vigenère
+ * 
+ * @param {string} iStr 
+ * @param {string} key
+ */
+function vigEncrypt(iStr, key) {
+    let string = iStr
+    key = key.toUpperCase()
+    let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let outputStr = ''
+    for (let i = 0; i < string.length; i++) {
+        // Get the current letter from the input string
+        const ele1 = string[i]
+        // Loop through all letters of the alphabet
+        for (let j = 0; j < letters.length; j++) {
+            const ele2 = letters[j]
+            if(ele1 == ele2) {
+                for (let k = 0; k < letters.length; k++) {
+                    console.log(key[i%3])
+                    if(key[i%3] == letters[k]) {
+                        newIndex = j + k
+                        // COMPLETE IT I ADDED THE INDEX!
+                    }
+                }
+            }
+            // If it didn't match then it must be a special character so just append the special character to the output string
+            else if (j == letters.length-1) outputStr += ele1 
+        }
+    }
+    // Wrap cipher text in ct tag for cipher text styling
+    idOutput.innerHTML = "<ct>" + outputStr + "</ct>";
+    idOutput.style.backgroundColor = 'red'
 }
 
 
